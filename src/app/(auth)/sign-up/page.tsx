7e46ab6cfec48b74f65612a1bd2525f0bf919c7c
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-07-23 09:11:24 +0300

    add authentication with sign up and sign in forms

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+
+const SignUpPage = () => {
+  console.log("Sign up view");
+
+  return <SignUpView />;
+};
+
+export default SignUpPage;
