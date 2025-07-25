51df5d0869be564512b8ed71f20a24777d5d5bcf
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-07-25 10:16:16 +0300

    fix typo errors

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -5,7 +5,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import { createTRPCContext } from "@trpc/tanstack-react-query";
 import { useState } from "react";
-import { makeQueryClient } from "./query-clint";
+import { makeQueryClient } from "./query-client";
 import type { AppRouter } from "./routers/_app";
 export const { TRPCProvider, useTRPC } = createTRPCContext<AppRouter>();
 let browserQueryClient: QueryClient;
