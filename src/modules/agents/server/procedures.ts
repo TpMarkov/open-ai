d99c0fac4d74f0897fbe8accfe8d3fd6c09fc182
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-07-25 15:45:57 +0300

    add create protected procedure including require name and instructions

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,16 +1,31 @@
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import {
+  createTRPCRouter,
+  baseProcedure,
+  protectedProcedure,
+} from "@/trpc/init";
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { TRPCError } from "@trpc/server";
+import { agentsInsertSchema } from "../schemas";
 
 export const agentsRouter = createTRPCRouter({
+  // GET many agents API to use protectedProcedure
   getMany: baseProcedure.query(async () => {
     const data = await db.select().from(agents);
 
-    // await new Promise((resolve) => setTimeout(resolve, 5000));
-
-    // throw new TRPCError({ code: "BAD_REQUEST" });
-
     return data;
   }),
+
+  create: protectedProcedure
+    .input(agentsInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createdAgent] = await db
+        .insert(agents)
+        .values({
+          ...input,
+          userId: ctx.auth.user.id,
+        })
+        .returning();
+
+      return createdAgent;
+    }),
 });
