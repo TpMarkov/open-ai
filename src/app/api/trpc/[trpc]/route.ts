222a765d75d85402dfab8936286f4c5f79d6676b
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-07-25 10:32:33 +0300

    solve merge conflict

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,10 +1,7 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { createTRPCContext } from "@/trpc/init";
 import { appRouter } from "@/trpc/routers/_app";
-<<<<<<< HEAD
-=======
 
->>>>>>> f4b56b36c7938c0f34c687ec6a38bd128eafa51d
 const handler = (req: Request) =>
   fetchRequestHandler({
     endpoint: "/api/trpc",
