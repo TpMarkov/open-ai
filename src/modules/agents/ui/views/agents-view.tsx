71eb10f9f101ea5a5662301a341f486e9444f66b
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-07-25 13:05:19 +0300

    09.agents-setup

---
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+
+import React from "react";
+import { LoadingState } from "@/components/loading-state";
+import { ErrorState } from "@/components/error-state";
+
+const AgentsView = () => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+
+  return <div>{JSON.stringify(data, null, 2)}</div>;
+};
+
+export default AgentsView;
+
+export const AgentsLoadingView = () => {
+  return (
+    <LoadingState
+      title="Loading agents"
+      description="Please wait this might take a few seconds"
+    />
+  );
+};
+
+export const AgentsViewError = () => {
+  return (
+    <ErrorState
+      title="Error Loading Agents"
+      description="Something went wrong"
+    />
+  );
+};
